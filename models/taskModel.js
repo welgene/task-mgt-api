d02477233bdd4a2692dceeea777fd626792e4006
welgene <welgene6@gmail.com>
2025-09-30 05:00:22 +0300

    feat (update) :  redefine task model for better interactions with the database.
    -updated the taskModel.js file by redefining the structure of the data passed to the database.
    - included a title,description and completed of type string and a timestamp to record when the task was created.

--- models/taskModel.js
+++ models/taskModel.js
@@ -1,16 +1,21 @@
 const mongoose = require('mongoose');
 
-const taskSchema = mongoose.Schema(
-    {
-        title: {
-            type: String,
-            required: [true, 'Please add a title'],
-        },
-        description: {
-            type: String,
-        },
-    }, {
-        timestamps: true,
-    });
+const taskSchema = new mongoose.Schema({
+    title : {
+        type:String,
+        required:true,
+        trim: true
+    },
+    description: {
+        type:String
+    },
+    completed :{
+        type: String,
+        default: false
+    }
+ },  {
+        timestamps:true
+    }
+)
 
 module.exports = mongoose.model('Task', taskSchema);
