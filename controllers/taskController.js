5300e10d07d109e0073acc7cb072365a73b96464
welgene <welgene6@gmail.com>
2025-10-01 05:53:38 +0300

    update(business logic ) : redefine task control logic
    - Redefined the followig crud operations : getTasks, createTask, getTask, updateTask and deleteTask by  adding business logic instead of placeholder statements.
    -Added  try-catch block and also implementedstatus codes within the various crud operations to ensure proper error handling.
    - Imported taskModel module to ensure the implemented tasks crud operations interact with an actual database.
    
    -These changes in the taskController.js file to ensure it perfoms effectively.

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,46 +1,89 @@
+const HTTP_STATUS = require('../constants');
+const Task = require('../models/taskModel');
+
+
 /**
  * @desc    Get all tasks
  * @route   GET /api/tasks
  */
-const getTasks = (req, res) => {
-    const tasks = () => tasks
-    res.status(200).json({ message: 'Get all tasks' });
-};
+const getTasks = async (req,res)=>{
+    try{
+        const tasks = await Task.find();
+        res.status(HTTP_STATUS.OK).json(tasks);
+    }catch(err){
+        res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({message:err.message})
+    }
+}
 
 /**
  * @desc    Create a new task
  * @route   POST /api/tasks
  */
-const createTask = (req, res) => {
-    // Basic validation
-    if (!req.body.title) {
-        return res.status(400).json({ message: 'Please add a title for the task' });
-    }
-    res.status(201).json({ message: 'Create a new task' });
+const createTask = async (req, res) => {
+   try {
+         // basic validation
+    if(!req.body.title){
+        res.status(HTTP_STATUS.BAD_REQUEST).json({ message :"title is required"})
+    } 
+    // create new task
+    const newTask = await new Task({
+        title : req.body.title,
+        description : req.body.description,
+        completed : req.body.completed || false
+    })
+
+    //save created task
+    const savedTask = await newTask.save();
+    res.status(201).json({savedTask})
+   } catch (error) {
+        res.status(HTTP_STATUS.BAD_REQUEST)
+   }
+
 };
 
 /**
  * @desc    Get a single task
  * @route   GET /api/tasks/:id
  */
-const getTask = (req, res) => {
-    res.status(200).json({ message: `Get task ${req.params.id}` });
+const getTask = async  (req, res) => {
+    try {
+        
+        const task = await Task.findById(req.params.id)
+        if(!task) return res.status(HTTP_STATUS.NOT_FOUND).send('task does not exist')
+        res.status(HTTP_STATUS.OK).json(task)
+    } catch (err) {
+        res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({message : err.message})
+    }
 };
 
 /**
  * @desc    Update a task
  * @route   PUT /api/tasks/:id
  */
-const updateTask = (req, res) => {
-    res.status(200).json({ message: `Update task ${req.params.id}` });
+const updateTask = async (req, res) => {
+    try {
+        const updatedTask = Task.findByIdAndUpdate(req.params.id, 
+        req.body,
+        {new :true,runValidators: true}) 
+    if(!updateTask) return res.status(HTTP_STATUS.NOT_FOUND).json({message : "task not found"})
+    res.status(HTTP_STATUS.OK).json(updatedTask)
+    } catch (err) {
+        res.status(HTTP_STATUS.BAD_REQUEST).json({message : err.message})
+    }
 };
 
 /**
  * @desc    Delete a task
  * @route   DELETE /api/tasks/:id
  */
-const deleteTask = (req, res) => {
-    res.status(200).json({ message: `Delete task ${req.params.id}` });
+const deleteTask = async (req, res) => {
+    try {
+        const taskToDelete = await Task.findByIdAndDelete(req.params.id)
+        if(!taskToDelete) return res.status(HTTP_STATUS.NOT_FOUND).json({message : "task not found"})
+        res.status(HTTP_STATUS.OK).json(`task deleted ${req.params.id}`)
+    } catch (err) {
+        res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({message : err.message})
+    }
 };
 
 module.exports = {
